64494b9f80e6d49a05e81ed18437d4d1145f6a5a
Ansh sharma <ansh11639@gmail.com>
2025-04-21 15:02:43 +0530

    updated Doubly list

--- Linked-List/3.Doubly-List.cpp
+++ Linked-List/3.Doubly-List.cpp
@@ -1,4 +1,4 @@
-// Implementation of Doubly Linked-list
+// Implementation of Doubly Linked-list : All CRUD operation
 /*
 Structure of doubly linked-list (mota-mota🙏🏻)
 ex : head->1-><-2-><-3->nullptr
@@ -9,122 +9,121 @@ using namespace std;
 
 int length = 0;
 
-class D_node{
+class D_node {
 public:
     int data;
-    D_node* left;
-    D_node* right;
+    D_node* prev;
+    D_node* next;
 
-    D_node(int data){
+    D_node(int data) {
         this->data = data;
-        this->left = nullptr;
-        this->right = nullptr;
+        this->prev = nullptr;
+        this->next = nullptr;
     }
 };
 
 void insertAtBegin(D_node* &head, D_node* &tail, int data) {
     D_node* node = new D_node(data);
     if (head == nullptr && tail == nullptr) {
-        length++;
         head = tail = node;
     } else {
-        node->right = head;
-        head->left = node;
+        node->next = head;
+        head->prev = node;
         head = node;
-        length++;
     }
-}
-
-void removeFromBegin(D_node* &head){
-    if(head == nullptr){
-        return;
-    }
-    D_node* curr = head;
-    head = head->right;
-    if(head != nullptr){
-        head->left = nullptr;
-    }
-    length--;
-    delete curr;
-}
-
-
-void insertAtEnd(D_node* &head, D_node* &tail, int data){
-    D_node* node = new D_node(data);
-    if(head == nullptr && tail == nullptr){
-        length++;
-        head = tail = node;
-    }else{
-        tail->right = node;
-        node->left = tail;
-        tail = node;
-        length++;
-    }
-}
-
-void removeFromEnd(D_node* &tail){
-    if(tail == nullptr){
-        return;
-    }
-    D_node* curr = tail;
-    tail = tail->left;
-    if(tail != nullptr){
-        tail->right = nullptr;
-    }
-    length--;
-    delete curr;
-}
-
-void insertAtPosition(D_node* &head, D_node* &tail, int pos, int data){
-    if(pos == 0){
-        insertAtBegin(head, tail, data);
-        return;
-    }
-    if(pos == length){
-        insertAtEnd(head, tail, data);
-        return;
-    }
-    if(pos < 0 || pos > length){
-        cout << "Invalid Index!" << endl;
-    }
-    D_node* curr = head;
-    D_node* newNode = new D_node(data);
-    for(int i = 0; i < pos - 1; i++){
-        curr = curr->right;
-    }
-    newNode->right = curr->right;
-    if(curr->right != nullptr){
-        curr->right->left = newNode;
-    }
-    curr->right = newNode;
-    newNode->left = curr;
     length++;
 }
 
-void traverseD_list(D_node* &head){
+void removeFromBegin(D_node* &head) {
+    if (head == nullptr) {
+        return;
+    }
+    D_node* curr = head;
+    head = head->next;
+    if (head != nullptr) {
+        head->prev = nullptr;
+    }
+    delete curr;
+    length--;
+}
+
+void insertAtEnd(D_node* &head, D_node* &tail, int data) {
+    D_node* node = new D_node(data);
+    if (head == nullptr && tail == nullptr) {
+        head = tail = node;
+    } else {
+        tail->next = node;
+        node->prev = tail;
+        tail = node;
+    }
+    length++;
+}
+
+void removeFromEnd(D_node* &tail) {
+    if (tail == nullptr) {
+        return;
+    }
+    D_node* curr = tail;
+    tail = tail->prev;
+    if (tail != nullptr) {
+        tail->next = nullptr;
+    }
+    delete curr;
+    length--;
+}
+
+void insertAtPosition(D_node* &head, D_node* &tail, int pos, int data) {
+    if (pos == 0) {
+        insertAtBegin(head, tail, data);
+        return;
+    }
+    if (pos == length) {
+        insertAtEnd(head, tail, data);
+        return;
+    }
+    if (pos < 0 || pos > length) {
+        cout << "Invalid Index!" << endl;
+        return;
+    }
+
+    D_node* curr = head;
+    D_node* newNode = new D_node(data);
+    for (int i = 0; i < pos - 1; i++) {
+        curr = curr->next;
+    }
+    newNode->next = curr->next;
+    if (curr->next != nullptr) {
+        curr->next->prev = newNode;
+    }
+    curr->next = newNode;
+    newNode->prev = curr;
+    length++;
+}
+
+void traverseD_list(D_node* &head) {
     D_node* curr = head;
 
     cout << "head -> ";
-    while(curr != nullptr){
+    while (curr != nullptr) {
         cout << curr->data << " <--> ";
-        curr = curr->right;
+        curr = curr->next;
     }
     cout << "nullptr" << endl;
     cout << "Current length of list : " << length << endl;
 }
 
-void reverseTraverse(D_node* &tail){
+void reverseTraverse(D_node* &tail) {
     D_node* curr = tail;
-    while(curr != nullptr){
+    cout << "tail -> ";
+    while (curr != nullptr) {
         cout << curr->data << " <--> ";
-        curr = curr->left;
+        curr = curr->prev;
     }
     cout << "nullptr" << endl;
     cout << "Current length of list : " << length << endl;
 }
 
-
-int main(){
+int main() {
     D_node* head = nullptr;
     D_node* tail = nullptr;
 
