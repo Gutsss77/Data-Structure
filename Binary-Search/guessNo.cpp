f345f4a9950d9493f7e5360554526a60d5b94517
Ansh sharma <ansh11639@gmail.com>
2025-05-13 10:11:19 +0530

    Added Binary-Search Folder for ques

---
+++ Binary-Search/guessNo.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+
+class GuessingGame {
+    int secret;
+public:
+    GuessingGame(int sec) : secret(sec) {}
+
+    int guess(int num) {
+        if (num > secret) return -1;
+        else if (num < secret) return 1;
+        else return 0;
+    }
+
+    int guessNumber(int n) {
+        int low = 1, high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int res = guess(mid);
+            if (res == 0) return mid;
+            else if (res == -1) high = mid - 1;
+            else low = mid + 1;
+        }
+        return -1;
+    }
+};
+
+int main() {
+    int secret = 77;
+    int n = 100;
+    GuessingGame gg(secret);
+    int found = gg.guessNumber(n);
+    cout << "The secret number is: " << found << endl;
+    return 0;
+}
